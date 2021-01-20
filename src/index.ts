82e048bb0ec9852c10482fad7d33c5d56c2439c1
Meet Dave <meet.dave@outlook.com>
2021-01-20 17:51:46 +0100

    feat: setup apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Typescript setup!')
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "echo Hello"
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }: {url: string}) => console.log(`server started at ${url}`));
